685305d3d4ab673a7cbad2b232d58847c5145579
Valeria <valeriaoshiro19@gmail.com>
2017-01-10 14:24:52 -0800

    Add easy and hard mode

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var colors = [];
 var squares = document.querySelectorAll('.square');
 var $squares = $('.square');
 var $resetBtn = $('.reset');
+var $feedback = $('.feedback');
+var $easyBtn = $('.easy');
+var $hardBtn = $('.hard');
+var $h1 = $('h1');
 var winnerColor = 0; 
 
 function makeRandomColors(numOfSquares){
@@ -14,36 +18,61 @@ function makeRandomColors(numOfSquares){
 	}
 }
 
-function makeRandomWinner(){
-	winnerColor = Math.floor(Math.random() * 6);
+function makeRandomWinner(numOfSquares){
+	winnerColor = Math.floor(Math.random() * numOfSquares);
 	$('span').text(colors[winnerColor]);
 }
 
 function fillSquares(){
-	for(var i = 0; i < squares.length; i++){
+	$squares.css('display', 'none');
+	for(var i = 0; i < colors.length; i++){
 		squares[i].style.background = colors[i];
+		squares[i].style.display = 'inline-block';
 	}
 }
 
-function init(){
-	makeRandomColors(6);
-	makeRandomWinner();
+function reset(numOfSquares){
+	makeRandomColors(numOfSquares);
+	makeRandomWinner(numOfSquares);
 	fillSquares();
+	$resetBtn.text('New colors');
+	$feedback.text('');
+	$h1.css('background', '#0088B5');
+}
+
+function init(){
+	reset(6);
 }
 
 init();
 
+
+/* ----------Event Listeners---------- */
+
 $squares.on('click', function(){
+	//if correct guess, change all squares background, h1 background
+	//and reset it
 	if($(this).css('background-color') === colors[winnerColor]){
 		$squares.css('background', colors[winnerColor]);
+		$h1.css('background', colors[winnerColor]);
+		$resetBtn.text('Play Again?');
+		$feedback.text('Correct');
 	}
+	//else, incorrect guess, keep guessing
 	else {
 		$(this).css('background', 'white');
+		$feedback.text('Try Again');
 	}
 });
 
-$resetBtn.on('click', function(){
-	makeRandomColors(6);
-	makeRandomWinner();
-	fillSquares();
+$resetBtn.on('click', function(){ 
+	reset(6);
+});
+
+$easyBtn.on('click', function(){
+	reset(3);
+});
+
+$hardBtn.on('click', function(){
+	reset(6);
 });
