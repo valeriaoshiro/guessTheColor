f68da78f8e9fa88a1e50553e51f576b0daa785a9
Valeria <valeriaoshiro19@gmail.com>
2017-01-09 16:04:06 -0800

    Clean code up

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 var colors = [];
 var squares = document.querySelectorAll('.square');
 var $squares = $('.square');
-var winnerColor = Math.floor(Math.random() * 6);
+var winnerColor = 0; 
 
-function makeColorSquares(numOfSquares){
+function makeRandomColors(numOfSquares){
 	for(var i = 0; i < numOfSquares; i++){
 		var one = Math.floor(Math.random() * 256);
 		var two = Math.floor(Math.random() * 256);
 		var three = Math.floor(Math.random() * 256);
-		//console.log('rgb(' + one + ', ' + two + ', ' + three + ')');
 		colors.push('rgb(' + one + ', ' + two + ', ' + three + ')');
 	}
-	
-
 }
 
-makeColorSquares(6);
-
-$('span').text(colors[winnerColor]);
-
-
-for(var i = 0; i < squares.length; i++){
-	squares[i].style.background = colors[i];
+function makeRandomWinner(){
+	winnerColor = Math.floor(Math.random() * 6);
+	$('span').text(colors[winnerColor]);
 }
+
+function fillSquares(){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = colors[i];
+	}
+}
+
+function init(){
+	makeRandomColors(6);
+	makeRandomWinner();
+	fillSquares();
+}
+
+init();
 
 $squares.on('click', function(event){
 	if($(this).css('background-color') === colors[winnerColor]){
